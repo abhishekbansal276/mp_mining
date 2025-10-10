186b0d49155dbd30b4b64397a06e2e662635c235
Abhishek Bansal <73697686+abhishekbansal276@users.noreply.github.com>
2025-10-10 09:20:46 +0530

    Change BOT_TOKEN to new authentication token
    Updated the bot token for authentication.

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from telegram.ext import (
 from fetch_emm11_data import fetch_emm11_data
 from pdf_gen import pdf_gen  # Generates PDFs and (expected) merged PDF
 
-BOT_TOKEN = '8414234561:AAHkVLYrVcl1q_TBwrwwai4jD6JlQ6w-aDw'
+BOT_TOKEN = '8244928494:AAFBv7GkQb4TyWmvb2uRmy-mE7YVgF0rxz0'
 
 ASK_START, ASK_END, ASK_DISTRICT = range(3)
 user_sessions = {}
